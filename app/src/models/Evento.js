cff51e0383969e37ac0fad83f8055dd0a89a6b18
GianpaoloDalini <123815404+GianpaoloDalini@users.noreply.github.com>
2023-11-25 19:45:49 +0100

    eyo

--- app/src/models/Evento.js
+++ app/src/models/Evento.js
@@ -1,10 +1,9 @@
 export default class Evento {
-    constructor(cliente, data, luogo, descrizione, isFesta, dipendenti) {
+    constructor(cliente, data, luogo, descrizione, dipendenti) {
       this.cliente = cliente;
       this.data = data;
       this.luogo = luogo;
       this.descrizione = descrizione;
-      this.isFesta = isFesta;
       this.dipendenti = dipendenti;
     }
   }
--- server/src/main/java/com/evento/EventoController.java
+++ server/src/main/java/com/evento/EventoController.java
@@ -3,6 +3,7 @@ package com.evento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,14 @@ public class EventoController {
     private EventoService eventoService;
 
     @PostMapping("/addevento")
+    @PreAuthorize("hasAuthority('PROPRIETARIO') || hasAuthority('REFERENTE')")
     public ResponseEntity<Evento> creaEvento(@RequestBody Evento evento) {
         Evento nuovoEvento = eventoService.addEvento(evento);
         return new ResponseEntity<>(nuovoEvento, HttpStatus.CREATED);
     }
 
     @PutMapping("/updateevento/{id}")
+    @PreAuthorize("hasAuthority('PROPRIETARIO') || hasAuthority('REFERENTE')")
     public ResponseEntity<Evento> aggiornaEvento(@PathVariable long id, @RequestBody Evento evento) {
         Evento eventoAggiornato = eventoService.aggiornaEvento(id, evento);
         if (eventoAggiornato != null) {
@@ -31,6 +34,7 @@ public class EventoController {
     }
 
     @DeleteMapping("/deleteevento/{id}")
+    @PreAuthorize("hasAuthority('PROPRIETARIO') || hasAuthority('REFERENTE')")
     public ResponseEntity<Void> eliminaEvento(@PathVariable long id) {
         boolean eliminato = eventoService.eliminaEvento(id);
         if (eliminato) {
